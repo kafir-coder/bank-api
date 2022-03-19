50dd2ea7e745c55f3ad94c397d625e81b7ae9317
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-19 13:10:59 +0100

    test: should call AccountRepository.exists

---
+++ src/domain/use-cases/impl/debitFromAccount-service.spec.ts
@@ -0,0 +1,45 @@
+import { IAddTransactionRepository } from '../../models/contracts/addTransaction-repository'
+import { IGetAccountRepository } from '../../models/contracts/getAccount-repository'
+import { AddTransactionParams } from '../../models/transaction'
+import { IDebitFromAccountService } from '../debitFromAccount-service'
+import { DebitFromAccountServiceImpl } from './debitFromAccount-service-impl'
+import { GetAccountRepositoryMock } from './mocks/createAccount-service'
+import { AddTransactionRepositoryMock } from './mocks/debitFromAccount-service'
+
+
+type SutTypes = {
+	sut: IDebitFromAccountService
+	getAccountRepository: IGetAccountRepository
+	addTransactionRepository: IAddTransactionRepository
+}
+const make_sut = (): SutTypes => {
+	const getAccountRepository = new GetAccountRepositoryMock()
+	const addTransactionRepository = new AddTransactionRepositoryMock()
+
+	const sut = new DebitFromAccountServiceImpl(
+		getAccountRepository,
+		addTransactionRepository
+	)
+
+	return {
+		sut, getAccountRepository, addTransactionRepository
+	}
+}  
+describe('DebitFromAccount usecase', () => {
+	
+	it('should call AccountRepository.exists',async () => {
+		const data: AddTransactionParams = {
+			account_id: 'some-id',
+			value: 20.4, 
+			type: 'debit'
+		}
+		const { sut, getAccountRepository } = make_sut() 
+
+		jest.spyOn(getAccountRepository, 'exists')
+
+		await sut.debitFromAccount(data)
+		expect(getAccountRepository.exists).toHaveBeenCalledTimes(1)
+	})
+
+	
+})
