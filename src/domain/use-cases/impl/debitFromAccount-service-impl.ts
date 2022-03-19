2bc23fa81ccced1e882551c47b58d3fce89113b5
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-19 13:10:42 +0100

    feat: should call AccountRepository.exists

---
+++ src/domain/use-cases/impl/debitFromAccount-service-impl.ts
@@ -0,0 +1,20 @@
+import {Adapter, Service} from '@tsclean/core'
+import { ADD_TRANSACTION_REPOSITORY, IAddTransactionRepository } from '../../models/contracts/addTransaction-repository'
+import { GET_ACCOUNT_REPOSITORY, IGetAccountRepository } from '../../models/contracts/getAccount-repository'
+import { AddTransactionParams, TransactionModel } from '../../models/transaction'
+import {IDebitFromAccountService} from '../../use-cases/debitFromAccount-service'
+
+@Service()
+export class DebitFromAccountServiceImpl implements IDebitFromAccountService {
+	constructor(
+		@Adapter(GET_ACCOUNT_REPOSITORY) private readonly getAccountRepository: IGetAccountRepository,
+		@Adapter(ADD_TRANSACTION_REPOSITORY) private readonly addTransactionRepository: IAddTransactionRepository
+	) {}
+
+	async debitFromAccount(data: AddTransactionParams): Promise<TransactionModel | null> {
+		
+		const { account_id } = data
+		this.getAccountRepository.exists(account_id)
+		return Object.assign({id: 'some-id'}, data)
+	}
+}
