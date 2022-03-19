8486169f6de4a8c669f8ce49c4d3bf5c2f1164a9
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-19 16:26:36 +0100

    chore: add update function to contract

--- src/domain/models/contracts/writeAccount-repository.ts
+++ src/domain/models/contracts/writeAccount-repository.ts
@@ -3,4 +3,5 @@ import { AccountModel, AddAccountParams } from '../account'
 export const ADD_ACCOUNT_REPOSITORY = 'CREATE_ACCOUNT_REPOSITORY'
 export interface ICreateAccountRepository {
     create(data: AddAccountParams): Promise<AccountModel>
+    update(data: Partial<AddAccountParams>): Promise<AccountModel>
 }
