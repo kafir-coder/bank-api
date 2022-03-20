43e5211317e5be3c8fa02a3f94de662b6020863f
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-20 01:22:18 +0100

    feat: should return null if the readAccountRepository.getBalance returns null

--- src/domain/models/contracts/readAccount-repository.ts
+++ src/domain/models/contracts/readAccount-repository.ts
@@ -2,5 +2,5 @@ export const READ_ACCOUNT_REPOSITORY = 'GET_ACCOUNT_REPOSITORY'
 export interface IReadAccountRepository {
   existsByCPF(cpf: string): Promise<boolean>
   exists(account_id: string): Promise<boolean>
-  getBalance(account_id: string): Promise<number>
+  getBalance(account_id: string): Promise<number | null>
 }
