175d62e3d07a70a33bcf1dedc4c5048ea2587446
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-20 09:18:18 +0100

    test: configure @shelf/mongodb for in-memory mongodb connection

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,33 @@
-describe('some test', () => {
-	it('should' , () => {
-		expect(2+2).toBe(4)
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-nocheck
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const {MongoClient} = require('mongodb')
+
+describe('insert', () => {
+	let connection
+	let db
+
+	beforeAll(async () => {
+		connection = await MongoClient.connect(global.__MONGO_URI__, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		})
+		db = await connection.db(global.__MONGO_DB_NAME__)
+	})
+
+	afterAll(async () => {
+		await connection.close()
+		await connection.close()
+	})
+
+	it('should insert a doc into collection', async () => {
+		const users = db.collection('users')
+
+		const mockUser = {_id: 'some-user-id', name: 'John'}
+		await users.insertOne(mockUser)
+
+		const insertedUser = await users.findOne({_id: 'some-user-id'})
+		console.log(insertedUser)
+		expect(insertedUser).toEqual(mockUser)
 	})
 })
