0c2a45005a85a2d89c2d5667328eabe471f54216
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-21 16:36:41 +0100

    chore: add AccountAlreadyExistsError Error

---
+++ src/domain/errors/account-already-exists-error.ts
@@ -0,0 +1,7 @@
+class AccountAlreadyExistsError extends Error {
+	constructor() {
+		// eslint-disable-next-line quotes
+		super(`Account already exists" }`)
+		this.name = 'AccountAlreadyExistsError'
+	}
+}
