c433334721ea67c293c1268246e6db2d6d7d9cc1
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-21 11:16:34 +0100

    feat: should only accepts transaction type as credit or debit

--- src/infrastructure/driven-adapters/adapters/orm/mongoose/models/transaction.ts
+++ src/infrastructure/driven-adapters/adapters/orm/mongoose/models/transaction.ts
@@ -4,7 +4,7 @@ import { model, Schema } from 'mongoose'
 const schema = new Schema<TransactionModel>({
 	account_id: {type: String, required: true},
 	amount: {type: Number, required: true},
-	type: {type: String, required: true}
+	type: {type: String, enum: ['credit', 'debit'], required: true}
 })
 
 export const TransactionModelSchema = model<TransactionModel>('transactions', schema)
