d55a58d80d76df750cf097df1b0e5c346e19382f
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-21 11:16:51 +0100

    test: should only accepts transaction type as credit or debit

--- src/infrastructure/driven-adapters/adapters/orm/mongoose/transaction-mongoose-repository-adapter.test.ts
+++ src/infrastructure/driven-adapters/adapters/orm/mongoose/transaction-mongoose-repository-adapter.test.ts
@@ -42,4 +42,60 @@ describe('Transaction mongo adapter',  () => {
 		expect(insertedTransaction.amount).toBe(wasInserted?.amount)
 		expect(insertedTransaction.type).toBe(wasInserted?.type)
 	})
+
+	describe('transaction type', () => {
+
+		const sut = make_sut()
+		class ValidationError extends Error {
+			constructor(type: string) {
+				super(`transactions validation failed: type: \`${type}\` is not a valid enum value for path \`type\`.`)
+				this.name = 'ValidationError'
+			}
+		}
+		const testcases = [
+			[
+				{account_id: '12345', type: 'panaceia', amount: 292.3}, 
+				new ValidationError('panaceia')
+			],
+			[
+				{account_id: '12345', type: 'credit', amount: 292.3}, 
+				(async function(id) { 
+					const wasInserted = await TransactionModelSchema.findOne({
+						_id: id
+					})
+					return wasInserted
+				})
+			],
+			[
+				{account_id: '12345', type: 'roraima', amount: 292.3}, 
+				new ValidationError('roraima')
+			],
+			[
+				{account_id: '12345', type: 'debit', amount: 292.3}, 
+				(async function(id) { 
+					const wasInserted = await TransactionModelSchema.findOne({
+						_id: id
+					})
+					return wasInserted
+				})
+			],
+			[
+				{account_id: '12345', type: 'lol', amount: 292.3}, 
+				new ValidationError('lol')
+			]
+		]
+		test.each(testcases)('should only accepts transaction type as credit or debit', async (transactiooParams, expected) => {
+			try {
+				const result = await sut.add(transactiooParams)
+				const expected_result = await expected(result.id)
+
+				expect(result.id).toBeTruthy()
+				expect(result.account_id).toBe(expected_result.account_id)
+				expect(result.amount).toBe(expected_result.amount)
+				expect(result.type).toBe(expected_result.type)
+			} catch (error) {
+				expect(error.name).toEqual(new ValidationError().name)
+			}
+		})
+	})
 })
