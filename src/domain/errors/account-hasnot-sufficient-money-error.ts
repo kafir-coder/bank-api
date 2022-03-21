dd9685a8d123f95bf1722e7832cef3f93622d486
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-21 16:39:29 +0100

    chore: add AccountHasNotSufficientMoneyError error

---
+++ src/domain/errors/account-hasnot-sufficient-money-error.ts
@@ -0,0 +1,7 @@
+class AccountHasNotSufficientMoneyError extends Error {
+	constructor() {
+		// eslint-disable-next-line quotes
+		super(`Account doesn't has sufficient money" }`)
+		this.name = 'AccountHasNotSufficientMoneyError'
+	}
+}
