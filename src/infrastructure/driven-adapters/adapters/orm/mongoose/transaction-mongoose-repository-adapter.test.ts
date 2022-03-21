e8fe3adb1f4835ecf068224eff3975245cfd8ae7
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-21 10:16:58 +0100

    test: should insert a doc into collection transaction

--- src/infrastructure/driven-adapters/adapters/orm/mongoose/transaction-mongoose-repository-adapter.test.ts
+++ src/infrastructure/driven-adapters/adapters/orm/mongoose/transaction-mongoose-repository-adapter.test.ts
@@ -3,7 +3,9 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 import { IWriteTransactionRepository } from '../../../../../domain/models/contracts/writeTransaction-repository'
 import { TransactionMongooseRepositoryAdapter } from './transaction-mongoose-repository-adapter'
-import {connect} from 'mongoose'
+import mongoose from 'mongoose'
+import { TransactionModelSchema } from './models/transaction'
+const {connect} = mongoose
 const make_sut = (): IWriteTransactionRepository => {
 	return new TransactionMongooseRepositoryAdapter()
 }
@@ -18,9 +20,26 @@ describe('Transaction mongo adapter',  () => {
 	})
 
 	beforeEach(async () => {
-		await AccountModelSchema.deleteMany({})
+		await TransactionModelSchema.deleteMany({})
 	})
 	afterAll(async () => {
 		await mongoose.connection.close()
+	})
+
+	it('should insert a doc into collection transaction', async () => {
+		const sut = make_sut()
+		
+		const account_id = '123456'
+		const amount = 200.2
+		const mockTransaction: AddTransactionParams = {account_id, type: 'debit', amount}
+		const insertedTransaction = await sut.add(mockTransaction)
+		const wasInserted = await TransactionModelSchema.findOne({
+			_id: insertedTransaction.id
+		})
+		
+		expect(insertedTransaction.id).toBeTruthy()
+		expect(insertedTransaction.account_id).toBe(wasInserted?.account_id)
+		expect(insertedTransaction.amount).toBe(wasInserted?.amount)
+		expect(insertedTransaction.type).toBe(wasInserted?.type)
 	})
 })
