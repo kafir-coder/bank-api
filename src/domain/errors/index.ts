759fa5e223a0ebe53c65ef9c42464b9603457c66
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-22 20:21:23 +0100

    refactor: move all errors to the same path

---
+++ src/domain/errors/index.ts
@@ -0,0 +1,4 @@
+export { AccountAlreadyExistsError } from './account-already-exists-error'
+export { AccountDoesntExistsError } from './account-doesnt-exists-error'
+export { AccountHasNotSufficientMoneyError } from './account-hasnot-sufficient-money-error'
+export { ServerError } from './server-error'
