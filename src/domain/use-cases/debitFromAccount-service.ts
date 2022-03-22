538a31f7f8f74c1b05f9ea029540cae8d0dca579
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-22 20:27:34 +0100

    chore: add service descriptor

--- src/domain/use-cases/debitFromAccount-service.ts
+++ src/domain/use-cases/debitFromAccount-service.ts
@@ -1,5 +1,6 @@
 import { AddTransactionParams, TransactionModel } from '../models/transaction'
 
+export const DEBIT_FROM_ACCOUNT_SERVICE = 'DEBIT_FROM_ACCOUNT_SERVICE'
 export interface IDebitFromAccountService {
   debitFromAccount(data: AddTransactionParams): Promise<TransactionModel | Error>
 }
