5859ec33e7e33aea672e25d97426af4a1a08fd5a
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-24 00:23:07 +0100

    feat: should return AccountDoesntExistError if readAccountRepository.existsByCPF false

--- src/domain/use-cases/impl/accountExistsByCpf-service-impl.ts
+++ src/domain/use-cases/impl/accountExistsByCpf-service-impl.ts
@@ -1,14 +1,17 @@
 import {Adapter, Service} from '@tsclean/core'
 import {IAccountExistsByCpfService} from '@/domain/use-cases/accountExistsByCpf-service'
 import { IReadAccountRepository, READ_ACCOUNT_REPOSITORY } from '@/domain/models/contracts/readAccount-repository'
+import { AccountDoesntExistsError } from '@/domain/errors'
 
 @Service()
 export class AccountExistsByCpfServiceImpl implements IAccountExistsByCpfService {
 	constructor(
         @Adapter(READ_ACCOUNT_REPOSITORY) private readonly readAccountRepository: IReadAccountRepository
 	) {}
-	async existsByCpf(cpf: string): Promise<boolean> {
-		this.readAccountRepository.existsByCPF(cpf)
+	async existsByCpf(cpf: string): Promise<boolean | Error> {
+		const exists = await this.readAccountRepository.existsByCPF(cpf)
+	
+		if (!exists) return new AccountDoesntExistsError()
 		return true
 	}
 }
