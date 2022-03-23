42d83ebf47b16bd8f7837d8dca8ea0d1fde48e3f
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-23 23:42:27 +0100

    refactor: refactor service

--- src/domain/use-cases/impl/createAccount-service-impl.ts
+++ src/domain/use-cases/impl/createAccount-service-impl.ts
@@ -2,21 +2,13 @@ import {Adapter, Service} from '@tsclean/core'
 import {ICreateAccountService} from '@/domain/use-cases/createAccount-service'
 import { IWriteAccountRepository, WRITE_ACCOUNT_REPOSITORY } from '../../models/contracts/writeAccount-repository'
 import { AddAccountParams, AccountModel } from '@/domain/models/account'
-import { IReadAccountRepository, READ_ACCOUNT_REPOSITORY } from '../../models/contracts/readAccount-repository'
-import { AccountAlreadyExistsError } from '@/domain/errors/account-already-exists-error'
 @Service()
 export class CreateAccountServiceImpl implements ICreateAccountService {
 	constructor(
-        @Adapter(WRITE_ACCOUNT_REPOSITORY) private readonly createAccountRepository: IWriteAccountRepository,
-				@Adapter(READ_ACCOUNT_REPOSITORY) private readonly getAccountRepository: IReadAccountRepository
+        @Adapter(WRITE_ACCOUNT_REPOSITORY) private readonly createAccountRepository: IWriteAccountRepository
 	) {}
 
 	async createAccountService(data: AddAccountParams): Promise<AccountModel | Error> {
-		const {cpf} = data
-		const already_exists = await this.getAccountRepository.existsByCPF(cpf)
-
-		if (already_exists) return new AccountAlreadyExistsError() // returns AccountAlreadyExistsError
-
 		const account = await this.createAccountRepository.create(data)
 		return account
 	} 
