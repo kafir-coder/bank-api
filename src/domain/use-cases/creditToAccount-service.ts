1c0449fd6f06723f0748899da998e82c39e3e214
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-23 11:29:32 +0100

    feat: export service name

--- src/domain/use-cases/creditToAccount-service.ts
+++ src/domain/use-cases/creditToAccount-service.ts
@@ -1,5 +1,6 @@
 import { AddTransactionParams, TransactionModel } from '../models/transaction'
 
+export const CREDIT_TO_ACCOUNT_SERVICE = 'CREDIT_TO_ACCOUNT_SERVICE'
 export interface ICreditToAccountService {
   creditToAccount(data: AddTransactionParams): Promise<TransactionModel | Error>
 }
