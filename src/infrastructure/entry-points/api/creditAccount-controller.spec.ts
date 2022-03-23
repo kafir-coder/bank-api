b28ef2c1150df4d5524aa7c7763f1633edb0d40e
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-23 12:13:12 +0100

    test: should returns 400 if CreditAccountService.creditToAccount returns AccountDoesntExistsErro

--- src/infrastructure/entry-points/api/creditAccount-controller.spec.ts
+++ src/infrastructure/entry-points/api/creditAccount-controller.spec.ts
@@ -1,3 +1,4 @@
+import { AccountDoesntExistsError } from '@/domain/errors'
 import { AddTransactionParams, TransactionModel } from '@/domain/models/transaction'
 import { ICreditToAccountService } from '@/domain/use-cases/creditToAccount-service'
 import { CreditAccountController, CreditAccountControllerParams } from './creditAccount-controller'
@@ -48,5 +49,21 @@ describe('CreditToAccount controller', () => {
 		await sut.creditToAccount(creditParams)
 
 		expect(creditFromAccountService.creditToAccount).toHaveBeenCalledWith({...creditParams, type: 'credit'})
+	})
+	
+	it('should returns 400 if CreditAccountService.creditToAccount returns AccountDoesntExistsError', async () => {
+		const { sut, creditFromAccountService } = make_sut()
+
+		const creditParams: CreditAccountControllerParams = {
+			account_id: 'some-id',
+			amount: 20.2
+		}
+		jest.spyOn(creditFromAccountService, 'creditToAccount').mockReturnValue(Promise.resolve(new AccountDoesntExistsError()))
+
+		try {
+			await sut.creditToAccount(creditParams)
+		} catch (error) {
+			expect(error.name).toBe('BadRequestException')
+		}
 	})
 })
