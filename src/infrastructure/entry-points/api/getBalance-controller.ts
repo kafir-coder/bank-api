43d02b1cb19f6d6d16c3934cc0a98f35928ee3cb
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-23 17:40:32 +0100

    feat: should return 200 if GetBalanceService.getBalance returns number

--- src/infrastructure/entry-points/api/getBalance-controller.ts
+++ src/infrastructure/entry-points/api/getBalance-controller.ts
@@ -1,6 +1,6 @@
 import { GET_BALANCE_SERVICE, IGetBalanceService } from '@/domain/use-cases/getBalance-service'
 import {Mapping, Get, HttpCode, Adapter, Query, BadRequestException} from '@tsclean/core'
-import { badRequest } from './helpers/http-helpers'
+import { badRequest, HttpResponse, ok } from './helpers/http-helpers'
 
 @Mapping('api/v1/getBalance')
 export class GetBalanceController {
@@ -12,19 +12,14 @@ export class GetBalanceController {
     // Example function
     @Get()
     @HttpCode(200)
-	async getBalance(@Query() query: GetBalanceControllerParams): Promise<GetBalanceControllerResponse> {
+	async getBalance(@Query() query: GetBalanceControllerParams): Promise<HttpResponse> {
 		const result = await this.getBalanceService.getBalance(query.account_id)
 		
 		const errors = ['AccountDoesntExistsError']
-		console.log(result)
-		console.log(result instanceof Error)
 		if (result instanceof Error) {
-			console.log(result)
 			if (errors.includes(result.name)) throw new BadRequestException(badRequest(result))
 		}
-		return {
-			balance: 20
-		}
+		return ok({balance: result})
 	}
 }
 
