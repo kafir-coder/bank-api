5efa5b7e70e2aa6ac85532664136d0db067ba3dd
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-24 11:30:49 +0100

    feat: should call AccountExistsService.exists

--- src/infrastructure/entry-points/api/debitAccount-controller.ts
+++ src/infrastructure/entry-points/api/debitAccount-controller.ts
@@ -1,3 +1,4 @@
+import { ACCOUNT_EXISTS_SERVICE, IAccountExistsService } from '@/domain/use-cases/accountExists-service'
 import { DEBIT_FROM_ACCOUNT_SERVICE, IDebitFromAccountService } from '@/domain/use-cases/debitFromAccount-service'
 import {Adapter, BadRequestException, Body, HttpCode, Mapping, Post} from '@tsclean/core'
 import { badRequest, HttpResponse, ok } from './helpers/http-helpers'
@@ -5,17 +6,15 @@ import { badRequest, HttpResponse, ok } from './helpers/http-helpers'
 @Mapping('/api/v1/debit')
 export class DebitAccountController {
 	constructor(
+				@Adapter(ACCOUNT_EXISTS_SERVICE) private readonly accountExistsService: IAccountExistsService,
         @Adapter(DEBIT_FROM_ACCOUNT_SERVICE) private readonly debitFromAccountService: IDebitFromAccountService
 	) {}
   @Post()
 	@HttpCode(200)
 	async debitFromAccount(@Body() data: DebitAccountControllerParams): Promise<HttpResponse> {
+		this.accountExistsService.exists(data.account_id)
 		const result = await this.debitFromAccountService.debitFromAccount({...data, type: 'debit'})
 
-		const errors = ['AccountDoesntExistsError', 'AccountHasNotSufficientMoneyError']
-		if (result instanceof Error) {	
-			if (errors.includes(result.name)) throw new BadRequestException(badRequest(result), result.message)
-		}
 		return ok(result)
 	}
 }
