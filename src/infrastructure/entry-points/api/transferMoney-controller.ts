7dd71e3634ff7548c325ca31363ce182c9f0bd26
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-24 11:05:08 +0100

    feat: should call AccountExistsService.exists 2 times

--- src/infrastructure/entry-points/api/transferMoney-controller.ts
+++ src/infrastructure/entry-points/api/transferMoney-controller.ts
@@ -1,3 +1,5 @@
+import { AccountDoesntExistsError } from '@/domain/errors'
+import { ACCOUNT_EXISTS_SERVICE, IAccountExistsService } from '@/domain/use-cases/accountExists-service'
 import { CREDIT_TO_ACCOUNT_SERVICE, ICreditToAccountService } from '@/domain/use-cases/creditToAccount-service'
 import { DEBIT_FROM_ACCOUNT_SERVICE, IDebitFromAccountService } from '@/domain/use-cases/debitFromAccount-service'
 import {Adapter, BadRequestException, Body, HttpCode, Mapping, Post} from '@tsclean/core'
@@ -6,12 +8,17 @@ import { badRequest, HttpResponse, ok } from './helpers/http-helpers'
 @Mapping('/api/v1/transfer')
 export class TransferMoneyController {
 	constructor(
+				@Adapter(ACCOUNT_EXISTS_SERVICE) private readonly accountExistsService: IAccountExistsService,
         @Adapter(DEBIT_FROM_ACCOUNT_SERVICE) private readonly debitFromAccountService: IDebitFromAccountService,
         @Adapter(CREDIT_TO_ACCOUNT_SERVICE) private readonly creditToAccountService: ICreditToAccountService
 	) {}
     @Post()
     @HttpCode(200)
 	async transferMoney(@Body() data: TransferControllerParams): Promise<HttpResponse> {
+
+		const origin_exists = await this.accountExistsService.exists(data.origin_account_id)
+		const target_exists = await this.accountExistsService.exists(data.target_account_id)
+					
 		const {origin_account_id, target_account_id, amount} = data
 		const result = await this.debitFromAccountService.debitFromAccount({
 			amount,
