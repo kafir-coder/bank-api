421ed3808094fe2c77e31bfcc4ed9a2f837781b5
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-24 11:42:09 +0100

    feat: add transfeMoney controller

--- src/infrastructure/entry-points/api/index.ts
+++ src/infrastructure/entry-points/api/index.ts
@@ -2,10 +2,12 @@ import { CreateAccountController } from './createAccount-controller'
 import { CreditAccountController } from './creditAccount-controller'
 import { DebitAccountController } from './debitAccount-controller'
 import { GetBalanceController } from './getBalance-controller'
+import { TransferMoneyController } from './transferMoney-controller'
 
 export const controllers = [
 	DebitAccountController,
 	CreditAccountController,
 	GetBalanceController,
-	CreateAccountController
+	CreateAccountController,
+	TransferMoneyController
 ]
