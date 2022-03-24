af59f8e77c8fb2254e967ff3991f17bc51903e05
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-24 11:23:12 +0100

    chore: add AccountExitsService to providers

--- src/infrastructure/driven-adapters/providers/index.ts
+++ src/infrastructure/driven-adapters/providers/index.ts
@@ -1,11 +1,13 @@
 import { READ_ACCOUNT_REPOSITORY } from '@/domain/models/contracts/readAccount-repository'
 import { WRITE_ACCOUNT_REPOSITORY } from '@/domain/models/contracts/writeAccount-repository'
 import { WRITE_TRANSACTION_REPOSITORY } from '@/domain/models/contracts/writeTransaction-repository'
+import { ACCOUNT_EXISTS_SERVICE } from '@/domain/use-cases/accountExists-service'
 import { ACCOUNT_EXISTS_BY_CPF_SERVICE } from '@/domain/use-cases/accountExistsByCpf-service'
 import { CREATE_ACCOUNT_SERVICE } from '@/domain/use-cases/createAccount-service'
 import { CREDIT_TO_ACCOUNT_SERVICE } from '@/domain/use-cases/creditToAccount-service'
 import { DEBIT_FROM_ACCOUNT_SERVICE } from '@/domain/use-cases/debitFromAccount-service'
 import { GET_BALANCE_SERVICE } from '@/domain/use-cases/getBalance-service'
+import { AccountExistsServiceImpl } from '@/domain/use-cases/impl/accountExists-service-impl'
 import { AccountExistsByCpfServiceImpl } from '@/domain/use-cases/impl/accountExistsByCpf-service-impl'
 import { CreateAccountServiceImpl } from '@/domain/use-cases/impl/createAccount-service-impl'
 import { CreditToAccountServiceImpl } from '@/domain/use-cases/impl/creditToAccount-service-impl'
@@ -51,5 +53,9 @@ export const services = [
 	{
 		provide: ACCOUNT_EXISTS_BY_CPF_SERVICE,
 		useClass: AccountExistsByCpfServiceImpl
+	},
+	{
+		provide: ACCOUNT_EXISTS_SERVICE,
+		useClass: AccountExistsServiceImpl,
 	}
 ]
