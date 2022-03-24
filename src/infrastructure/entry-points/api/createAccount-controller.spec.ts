0a8958cb1f7e99d1fd8d1a27ef9c11220fcb0ba9
Caio Tony <72797921+kafir-coder@users.noreply.github.com>
2022-03-24 01:10:10 +0100

    test: should call accountExistsByCpfService.existsByCpf

--- src/infrastructure/entry-points/api/createAccount-controller.spec.ts
+++ src/infrastructure/entry-points/api/createAccount-controller.spec.ts
@@ -1,6 +1,6 @@
 import { IAccountExistsByCpfService } from '@/domain/use-cases/accountExistsByCpf-service'
 import { ICreateAccountService } from '@/domain/use-cases/createAccount-service'
-import { CreateAccountController } from './createAccount-controller'
+import { CreateAccountController, CreateAccountRequest } from './createAccount-controller'
 import { AccountExistsByCpfServiceMock, CreateAccountServiceMock } from './mocks'
 
 type SutTypes = {
@@ -23,4 +23,21 @@ const make_sut = (): SutTypes => {
 }
 
 
-describe('createAccount controller', () => {})
+describe('createAccount controller', () => {
+
+	it('should call accountExistsByCpfService.existsByCpf', async () => {
+		const { sut, accountExistsByCpf } = make_sut()
+
+		const accountParams: CreateAccountRequest = {
+			owner_name: 'Caio Tony',
+			cpf: 'some-cpf',
+			initial_amount: 300
+		}
+
+		jest.spyOn(accountExistsByCpf, 'existsByCpf')
+
+		await sut.createAccount(accountParams)
+
+		expect(accountExistsByCpf.existsByCpf).toHaveBeenCalledTimes(1)
+	})
+})
